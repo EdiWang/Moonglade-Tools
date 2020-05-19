ea1b2ce397ee09fa24241e6f305d6cf9aff18531
Edi Wang <Edi.Wang@outlook.com>
2020-05-19 13:22:10 +0800

    HtmlEncodingRemoval framework

--- HtmlEncodingRemoval/Program.cs
+++ HtmlEncodingRemoval/Program.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using CommandLine;
 
 namespace HtmlEncodingRemoval
 {
+    class Options
+    {
+        [Option('c', Required = true, HelpText = "SQL Server Connection String")]
+        public string ConnectionString { get; set; }
+    }
+
     class Program
     {
+        public static Options Options { get; set; }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
