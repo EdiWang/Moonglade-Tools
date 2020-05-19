c1730eefb82c42357ca615e2de66df682df9e257
Edi Wang <Edi.Wang@outlook.com>
2020-05-19 14:01:56 +0800

    Decode posts

--- HtmlEncodingRemoval/Program.cs
+++ HtmlEncodingRemoval/Program.cs
@@ -14,7 +14,7 @@ namespace HtmlEncodingRemoval
         public string ConnectionString { get; set; }
     }
 
-    class PageModel
+    class HtmlEncodedItem
     {
         public Guid Id { get; set; }
         public string HtmlContent { get; set; }
@@ -36,7 +36,10 @@ namespace HtmlEncodingRemoval
                 WriteMessage($"Connecting to database.", ConsoleColor.Gray);
 
                 await using var conn = new SqlConnection(Options.ConnectionString);
-                var pages = (await conn.QueryAsync<PageModel>("SELECT Id, HtmlContent FROM CustomPage cp")).ToList();
+                WriteMessage($"Connected to database.", ConsoleColor.Gray);
+
+                var pages = (await conn.QueryAsync<HtmlEncodedItem>(
+                    "SELECT Id, HtmlContent FROM CustomPage cp")).ToList();
                 foreach (var page in pages)
                 {
                     var decodedHtml = HttpUtility.HtmlDecode(page.HtmlContent);
@@ -45,8 +48,15 @@ namespace HtmlEncodingRemoval
                 }
                 WriteMessage($"Custom Pages Decoded, total {pages.Count} item(s).", ConsoleColor.Gray);
 
-
-                WriteMessage($"Connected to database.", ConsoleColor.Gray);
+                var posts = (await conn.QueryAsync<HtmlEncodedItem>(
+                    "SELECT Id, PostContent AS HtmlContent FROM Post p")).ToList();
+                foreach (var post in posts)
+                {
+                    var decodedHtml = HttpUtility.HtmlDecode(post.HtmlContent);
+                    var sqlPosts = "UPDATE Post SET PostContent = @decodedHtml WHERE Id = @postId";
+                    await conn.ExecuteAsync(sqlPosts, new { decodedHtml, postId = post.Id });
+                }
+                WriteMessage($"Posts Decoded, total {posts.Count} item(s).", ConsoleColor.Gray);
             }
 
             WriteMessage($"Done.", ConsoleColor.Green);
